3967de4e27f8d4967c8b6c0004d5f197afe1c09d
LeeBate <harukaxd02@gmail.com>
2025-05-24 21:10:47 +0700

    log

--- app/services/onetouchService.ts
+++ app/services/onetouchService.ts
@@ -5,7 +5,7 @@ import { OneTouch } from "@/types/onetouch";
 export async function getOneTouchFromApi(): Promise<OneTouch[]> {
   const url = getApiUrl("/api/get-onetouch");
   const res = await fetch(url, { cache: "no-store" });
-// console.log("res",res)
+console.log("res->>>>>>>>>>>>>>>>>>",res)
   if (!res.ok) throw new Error("Failed to fetch OneTouch data");
 
   return res.json();
