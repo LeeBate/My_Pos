979d5f2176e8f935e56b37ca18ccb74a1283d1c3
LeeBate <harukaxd02@gmail.com>
2025-05-24 20:24:47 +0700

    log

--- app/one-touch/components/left-section.tsx
+++ app/one-touch/components/left-section.tsx
@@ -4,6 +4,8 @@ import LeftSectionClient from "./left-section-client"; // import client componen
 const LeftSection = async () => {
   try {
     const oneTouchInfo = await getOneTouchFromApi();
+
+    console.log('oneTouchInfo@@',oneTouchInfo)
     return <LeftSectionClient oneTouchInfo={oneTouchInfo} />;
   } catch (error) {
     return <div>Error fetching data</div>;
