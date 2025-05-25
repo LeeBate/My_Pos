8b489f091484953ee73fb4b267d91ac5e9c0b5c9
LeeBate <harukaxd02@gmail.com>
2025-05-25 12:53:53 +0700

    viewpoint

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      {/* สามารถเพิ่ม <title> หรือ <meta> อื่น ๆ ได้ที่นี่ */}
+    </>
+  );
+}
